8296cb364d4c3a68d90e979c3f26b325c2451fd9
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-06 03:04:57 +0100

    db connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // ** Imports
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -13,7 +14,23 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send(`Rostocker events server running`));
 
+// ** DB CONNECTION
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ikallh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 // ** app listen
 app.listen(port, () => {
+  client.connect((err) => {
+    if (err) {
+      console.log("DB Error Happened");
+    }
+    console.log("DB Connected");
+  });
   console.log(`Server is running on port: ${port}`);
 });
